7d49f45824b008dc0dd87102d5f1235937359eca
Franco Serio <francoagustinserio@gmail.com>
2017-07-23 12:12:25 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
